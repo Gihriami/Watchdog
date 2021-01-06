dfcdb56804ecfd7d3300f7fd8ea4ac6498efdbf1
gihirami <gihriami@protonmail.com>
2021-01-06 07:55:09 -0800

    correction

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import time
 import os
 import json
 
+
 # creation de fonction
 def ask(answer):
-    os.system('grep '+answer+' watchdog.conf | cut -d \\" -f 4')
+    response = os.system('grep ' + answer + ' watchdog.conf | cut -d \\" -f 4')
+    return response
+
 
 def getsize():
     os.system('grep Name watchdog.conf | cut -d \\" -f 4 | wc -l')
@@ -13,37 +16,34 @@ def getsize():
 
 if __name__ == "__main__":
     # recuperation des process
-    #os.system('ps eaxco command')
+    # os.system('ps eaxco command')
     # lecture du fichier conf voir qui est true ou false
-    #controle = ask("'enable'")
-    #if controle ==
+    # controle = ask("'enable'")
+    # if controle ==
     controleName = ask("'Name'")
     controleValue = ask("'enable'")
-    sizelist=getsize()
+    sizelist = getsize()
 
-    #1e étape
+    # 1e étape
     liste1 = []
     liste2 = []
 
-    #2e étape
-    for i in range(sizelist):
-        liste1.append(input(controleName))
+    # 2e étape
+    for i in range(2):
+        liste1.append(controleName)
 
-    #3e étape
+    # 3e étape
     print("Your first list : ", liste1)
 
+    # def myfunc(a, b):
+    #   return a + b
 
-    #def myfunc(a, b):
-     #   return a + b
+#  print(liste1)
+# print(liste2)
 
-  #  print(liste1)
-   # print(liste2)
+# x = map(myfunc, liste1, liste2)
 
-    #x = map(myfunc, liste1, liste2)
+# print(x)
 
-    #print(x)
-
-    # convert the map into a list, for readability:
-    #print(list(x))
-
-
+# convert the map into a list, for readability:
+# print(list(x))
