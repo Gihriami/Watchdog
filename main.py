cdb0aaa6030143edda54cc9fdf0a1eb83310a596
gihirami <gihriami@protonmail.com>
2021-01-06 05:48:43 -0800

    Oui

---
+++ main.py
@@ -0,0 +1,21 @@
+# import
+import time
+import os
+import json
+
+# creation de fonction
+def ask(answer):
+    os.system('grep '+answer+' watchdog.conf | cut -d \\" -f 4')
+
+def tab():
+    for cptligne+1 in controle :
+        if controle == 'true':
+            tab[i]
+
+if __name__ == "__main__":
+    # recuperation des process
+    #os.system('ps eaxco command')
+    # lecture du json
+    controle = ask("'Name\\|enable'")
+    #if controle ==
+    foreach controle :
