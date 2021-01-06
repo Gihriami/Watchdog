353330fcb67100f248bd55030f969702fbda0dd1
gihirami <gihriami@protonmail.com>
2021-01-06 06:17:45 -0800

    correction

--- main.py
+++ main.py
@@ -8,14 +8,12 @@ def ask(answer):
     os.system('grep '+answer+' watchdog.conf | cut -d \\" -f 4')
 
 def tab():
-    for cptligne+1 in controle :
-        if controle == 'true':
-            tab[i]
+
 
 if __name__ == "__main__":
     # recuperation des process
     #os.system('ps eaxco command')
-    # lecture du json
-    controle = ask("'Name\\|enable'")
+    # lecture du fichier conf voir qui est true ou false
+    controle = ask("'enable'")
     #if controle ==
-    foreach controle :
+
