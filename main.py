0b6302f7302179cac34f867fc42985005eb21f43
nathueur <36504824+nathueur@users.noreply.github.com>
2021-01-06 07:40:13 -0800

    Tentative de Map Nom + enable
    ça march po

--- main.py
+++ main.py
@@ -7,13 +7,43 @@ import json
 def ask(answer):
     os.system('grep '+answer+' watchdog.conf | cut -d \\" -f 4')
 
-def tab():
+def getsize():
+    os.system('grep Name watchdog.conf | cut -d \\" -f 4 | wc -l')
 
 
 if __name__ == "__main__":
     # recuperation des process
     #os.system('ps eaxco command')
     # lecture du fichier conf voir qui est true ou false
-    controle = ask("'enable'")
+    #controle = ask("'enable'")
     #if controle ==
+    controleName = ask("'Name'")
+    controleValue = ask("'enable'")
+    sizelist=getsize()
+
+    #1e étape
+    liste1 = []
+    liste2 = []
+
+    #2e étape
+    for i in range(sizelist):
+        liste1.append(input(controleName))
+
+    #3e étape
+    print("Your first list : ", liste1)
+
+
+    #def myfunc(a, b):
+     #   return a + b
+
+  #  print(liste1)
+   # print(liste2)
+
+    #x = map(myfunc, liste1, liste2)
+
+    #print(x)
+
+    # convert the map into a list, for readability:
+    #print(list(x))
+
 
