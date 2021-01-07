bec72da4be2e1aaba9e2e5d8c884bb92da317fd8
nathueur <36504824+nathueur@users.noreply.github.com>
2021-01-07 16:37:24 +0100

    Update nathmain.py

--- nathmain.py
+++ nathmain.py
@@ -7,9 +7,32 @@ with open('watchdog.conf') as json_file:
 
 
 def getWithKey(key):
+    b=[]
     for i in range(len(data)):
-        print(data[i][key])
+        #print(data[i][key])
+        temp=data[i][key]
+        #print(temp)
+        b.append(temp)
+    return b
 
-getWithKey("Name")
+#a=getWithKey("Name")
+
+#for i in range(len(a)):
+#    print(a[i])
+
+def checkIfTrue():
+    names=getWithKey("Name")
+    enabled=getWithKey("enable")
+    tab=[]
+    for i in range(len(names)):
+        if(enabled[i]=="true"):
+            tab.append(names[i])
+    return tab
+
+tableau=checkIfTrue()
+
+for i in range(len(tableau)):
+    print(tableau[i])
+
 
 json_file.close()
