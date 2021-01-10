86112f113b84326f59551d0a077aae75f200910c
nathueur <36504824+nathueur@users.noreply.github.com>
2021-01-10 23:07:25 +0100

    Update nathmain.py

--- nathmain.py
+++ nathmain.py
@@ -1,24 +1,15 @@
 # import
 import os
 import json
-
-with open('watchdog.conf') as json_file:
-    data = json.load(json_file)
-
+import subprocess
+import time
 
 def getWithKey(key):
     b=[]
     for i in range(len(data)):
-        #print(data[i][key])
         temp=data[i][key]
-        #print(temp)
         b.append(temp)
     return b
-
-#a=getWithKey("Name")
-
-#for i in range(len(a)):
-#    print(a[i])
 
 def checkIfTrue():
     names=getWithKey("Name")
@@ -29,10 +20,79 @@ def checkIfTrue():
             tab.append(names[i])
     return tab
 
-tableau=checkIfTrue()
+def getAllWatchtimeFromTrue():
+    names = getWithKey("watchtime")
+    enabled = getWithKey("enable")
+    tab = []
+    for i in range(len(names)):
+        if (enabled[i] == "true"):
+            tab.append(names[i])
+    return tab
 
-for i in range(len(tableau)):
-    print(tableau[i])
+#launchprocess
+def launchprocess(procss, mdp):
+
+    tabName=getWithKey("Name")
+    tabBin=getWithKey("binary")
+
+    bin="null"
+    for i in range (len(tabBin)):
+        if tabName[i]==procss:
+            bin=tabBin[i]
+
+    cmd= "echo "+ mdp +" |  sudo -S " + bin + " start"
+    os.system(cmd)
+
+def askPasswd():
+    print("Veuiilez entrer le mot de passe root : ")
+    mdp=input()
+    return mdp
 
 
-json_file.close()
+def relanceAllProcess(tps,mdp):
+    aRelancer = checkIfTrue()
+    tabletTrue=removePros()
+    allWatchtime=getAllWatchtimeFromTrue()
+    temps="{:.0f}".format(tps)
+
+    for i in range(len(aRelancer)):
+
+        for j in range(len(tabletTrue)):
+            regarderTemps= int(allWatchtime[i])
+            tempo=int(temps) % regarderTemps
+            if (tabletTrue[j]==aRelancer[i] and tempo==0):
+                launchprocess(aRelancer[i],mdp)
+
+
+
+def removePros():
+    tabletrue = checkIfTrue()
+    # recuperation des process
+    ps = subprocess.check_output(['ps', 'eaxco', 'command'], text=True)
+
+    # mise en place des process dans une liste
+    process = ps.split('\n')
+
+    # compare les valeurs pour resortir
+    for pro in process:
+        for valeur in tabletrue:
+            if valeur == pro:
+                tabletrue.remove(valeur)
+
+    return tabletrue
+
+
+if __name__ == "__main__":
+    with open('watchdog.conf') as json_file:
+        data = json.load(json_file)
+
+    mdp=askPasswd()
+
+    toc = time.perf_counter() % 3600
+    while True:
+        tic = time.perf_counter() % 3600 - toc + 1
+        time.sleep(1)
+        print("{:.0f}".format(tic))
+        relanceAllProcess(tic, mdp)
+
+    json_file.close()
